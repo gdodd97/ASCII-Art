af5335a59a054cc2cb05307c5339ee71d38301be
Greg Dodd <gdodd97@gmail.com>
2024-10-23 16:50:24 -0400

    Added tuple to array function. Looping through array is broken. Need to fix

--- asciiart.py
+++ asciiart.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 im = Image.open("cat.jpg")
 
 r,g,b = im.getpixel((0,0))
@@ -7,6 +8,23 @@ width = im.size[0]
 height = im.size[1]
 print("Successfully loaded image!")
 print("Image size:",width, "x",height)
-print("RGB Values in pixel 1:",r, g, b)
 
-#todo add rgb values for all pixels into 2D array
+def tuple_to_array(image):
+    image = image.convert('RGB')
+
+    width, height = image.size
+
+    rgb_array = np.zeros((height, width, 3), dtype=np.uint8)
+
+    for y in range(height):
+        for x in range(width):
+            rgb_array[y,x] = image.getpixel((x,y))
+    return rgb_array
+
+pixel_matrix = tuple_to_array(im)
+
+print("Iterating through RGB Values")
+for x in len(pixel_matrix):
+    for y in len(pixel_matrix[x]):
+        pixel = pixel_matrix[x][y]
+        print(pixel)
