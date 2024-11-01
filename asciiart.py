02cdbc2745564cb62ff86104ae5a85469f03197b
Greg Dodd <gdodd97@gmail.com>
2024-11-01 17:26:28 -0400

    Windows terminal sucks for resizing but it is working. Refactored code to not use numpy arrays. also added a normalize values function to help with the extremes that were causing issue.

--- asciiart.py
+++ asciiart.py
@@ -2,6 +2,8 @@ from PIL import Image
 import numpy as np
 im = Image.open("cat.jpg")
 
+ascii = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
 r,g,b = im.getpixel((0,0))
 
 width = im.size[0]
@@ -9,33 +11,56 @@ height = im.size[1]
 print("Successfully loaded image!")
 print("Image size:",width, "x",height)
 
-def tuple_to_array(image):
-    image = image.convert('RGB')
+def get_pixel_matrix(im, height):
+    im.thumbnail((height, 200))
+    pixels = list(im.getdata())
+    return [pixels[i:i+im.width] for i in range(0, len(pixels), im.width)]
 
-    width, height = image.size
 
-    rgb_array = np.zeros((height, width, 3), dtype=np.uint64)
 
-    for y in range(height):
-        for x in range(width):
-            rgb_array[y,x] = image.getpixel((x,y))
-    return rgb_array
-
-def array_to_brightness(image):
-    image = image.convert('RGB')
-    width, height = image.size
-    pixel_matrix = tuple_to_array(image)
+def array_to_brightness(pixel_matrix):
+    brightness_matrix = []
+    for row in pixel_matrix:
+        brightness_row = []
+        for pixel in row:
+            brightness = round(pixel[0] + pixel[1] + pixel[2] / 3.0)
+            brightness_row.append(brightness)
+        brightness_matrix.append(brightness_row)
     
-    brightness_matrix = np.zeros((height, width, 1), dtype=np.uint64)
-
-    for y in range(height):
-        for x in range(width):
-            brightness_matrix[y,x] = round(np.sum(pixel_matrix[y,x])/3)
-
     return brightness_matrix
 
-pixel_matrix = tuple_to_array(im)
-brightness_matrix = array_to_brightness(im)
+def normalize_brightness_matrix(brightness_matrix):
+    normal_brightness_array = []
+    max_pixel = max(map(max, brightness_matrix))
+    min_pixel = min(map(min, brightness_matrix))
+    for row in brightness_matrix:
+        rescaled_row = []
+        for p in row:
+            r = 255 * (p - min_pixel) / float(max_pixel - min_pixel)
+            rescaled_row.append(r)
+        normal_brightness_array.append(rescaled_row)
 
-print("Iterating through Brightness")
-print(brightness_matrix)
+    return normal_brightness_array
+
+def brightness_to_ascii(brightness_matrix, ascii):
+
+    ascii_matrix = []
+    for row in brightness_matrix:
+        ascii_row = []
+        for pixel in row:
+            ascii_row.append(ascii[int(pixel/255 * len(ascii)) - 1])
+        ascii_matrix.append(ascii_row)
+
+    return ascii_matrix
+
+def print_ascii_matrix(ascii_matrix):
+    for row in ascii_matrix:
+        line = [p+p+p for p in row]
+        print(line)
+
+pixels = get_pixel_matrix(im,1000)
+brightness_matrix = array_to_brightness(pixels)
+brightness_matrix = normalize_brightness_matrix(brightness_matrix)
+ascii_matrix = brightness_to_ascii(brightness_matrix, ascii)
+
+print_ascii_matrix(ascii_matrix)
