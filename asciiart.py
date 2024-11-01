db0de3427c1df3601913864b81d329181937f49d
Greg Dodd <gdodd97@gmail.com>
2024-11-01 16:06:42 -0400

    Got brightness matrix working and iterating.

--- asciiart.py
+++ asciiart.py
@@ -14,20 +14,28 @@ def tuple_to_array(image):
 
     width, height = image.size
 
-    rgb_array = np.zeros((height, width, 3), dtype=np.uint8)
+    rgb_array = np.zeros((height, width, 3), dtype=np.uint64)
 
     for y in range(height):
         for x in range(width):
             rgb_array[y,x] = image.getpixel((x,y))
     return rgb_array
 
-pixel_matrix = tuple_to_array(im)
+def array_to_brightness(image):
+    image = image.convert('RGB')
+    width, height = image.size
+    pixel_matrix = tuple_to_array(image)
+    
+    brightness_matrix = np.zeros((height, width, 1), dtype=np.uint64)
 
-print("Iterating through RGB Values")
-#for x in len(pixel_matrix):
-#    for y in len(pixel_matrix[x]):
-#        pixel = pixel_matrix[x][y]
-#        print(pixel)
-for row in pixel_matrix:
-    for element in row:
-        print(element)
+    for y in range(height):
+        for x in range(width):
+            brightness_matrix[y,x] = round(np.sum(pixel_matrix[y,x])/3)
+
+    return brightness_matrix
+
+pixel_matrix = tuple_to_array(im)
+brightness_matrix = array_to_brightness(im)
+
+print("Iterating through Brightness")
+print(brightness_matrix)
